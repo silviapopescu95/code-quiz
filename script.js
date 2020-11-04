00839af4b3f7c2c45cefa35287bdd46cfa1d2e51
Silvia Popescu <amazingaivlis@gmail.com>
2020-11-03 17:33:05 -0800

    Added DOM reference variable, started functions to start quiz and get questions.

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 var timerEl = document.querySelector(".timer");
+var startScreenEl = document.getElementById("#start-screen");
+var startButtonEl = document.getElementById("start-button");
+var questionsEl = document.querySelector(".questions");
+var questionTitleEl = document.getElementById("#question-title");
+var choicesEl = document.getElementById("#choices");
+
+var time = 90;
+var questionIndex = i;
+//var currentQuestion;
+
+function startQuiz() {
+    startScreenEl.addEventListener("click", function(event) {
+        event.preventDefault();
+        startScreenEl.style.display = "none";
+    })
+    getQuestion();
+}
+
+function getQuestion() {
+    var currentQuestion = questions[questionIndex];
+    questionTitleEl.textContent = currentQuestion.title;
+    console.log(currentQuestion);
+}
